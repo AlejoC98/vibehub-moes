1b40eca05124bd93e5f787e78ecd972d3e95f539
Alejandro Gomez <alegomezc98@icloud.com>
2025-04-08 21:11:21 -0500

    I added a suspense to the reset password page because during deployment I got an error

--- src/app/(public)/auth/reset-password/page.tsx
+++ src/app/(public)/auth/reset-password/page.tsx
@@ -3,7 +3,7 @@ import ResetPasswordForm from '@/components/forms/reset_password_form'
 import UpdatePasswordForm from '@/components/forms/update_password_form';
 import { Box, Typography } from '@mui/material'
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React, { Suspense, useEffect } from 'react'
 
 const ResetPassword = () => {
 
@@ -11,15 +11,17 @@ const ResetPassword = () => {
   const code = searchParams.get('code');
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', placeItems: 'center', minHeight: '100vh'}}>
-      <Box sx={{ background: 'rgba(29, 38, 51, .7)', borderRadius: 5, maxWidth: 500}}>
-        { code == null ? (
-          <ResetPasswordForm />
-        ) : (
-          <UpdatePasswordForm />
-        ) }
+    <Suspense fallback={<div>Loading...</div>}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', placeItems: 'center', minHeight: '100vh'}}>
+        <Box sx={{ background: 'rgba(29, 38, 51, .7)', borderRadius: 5, maxWidth: 500}}>
+          { code == null ? (
+            <ResetPasswordForm />
+          ) : (
+            <UpdatePasswordForm />
+          ) }
+        </Box>
       </Box>
-    </Box>
+    </Suspense>
   )
 }
 
