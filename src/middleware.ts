85c23cd64e576d5bc3aad5ec09735b35b1244392
Alejandro Gomez <alegomezc98@icloud.com>
2025-04-09 09:57:00 -0500

    I'm working in the new filter for the middleware to do not allow users to some pages base on their roles

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,27 +4,51 @@ import { updateSession } from '@/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
 
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const publicUrls = ['/', '/auth/reset-password', '/auth/login'];
+  var userData = null;
+  const destinyPath = request.nextUrl.pathname;
+  const { data: { session } } = await supabase.auth.getSession();
+  const publicUrls = ['/', '/auth/reset-password', '/auth/login'];
+  
 
-    if (publicUrls.includes(request.nextUrl.pathname)) {
-      return await updateSession(request);
+  if (session) {
+    const { data: userQuery, error } = await supabase
+    .from('accounts')
+    .select('*, locations!accounts_location_id_fkey(*), accounts_roles(*, roles(id, name))')
+    .eq('user_id', session?.user.id)
+    .single();
+
+    if (error) {
+      await supabase.auth.signOut();
+      return NextResponse.redirect(new URL('/auth/login', request.url));
     }
 
-    const { data: { session } } = await supabase.auth.getSession();
+    userData = userQuery;
+  }
 
-    const isAuthPage = request.nextUrl.pathname.startsWith('/auth/login');
-
-    if (!isAuthPage) {
-      if (!session) {
-        return NextResponse.redirect(new URL('/auth/login', request.url));
-      }
-    } else {
+  switch (destinyPath) {
+    case '/auth/login':
       if (session) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
       }
-    }
+      break;
+    case '/dashboard':
+      if (!session) {
+        return NextResponse.redirect(new URL('/auth/login', request.url));
+      }
+      break;
+    case '/users':
+      if (!userData.accounts_roles.includes(2)) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+      }
+    break;
+    default:
+      if (publicUrls.includes(request.nextUrl.pathname)) {
+        return await updateSession(request);
+      }
+      break;
+  }
 
   return await updateSession(request)
 }
