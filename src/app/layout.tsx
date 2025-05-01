931bdf9b91ae8f605cdc01559176c5dd82590ddc
Alejandro Gomez <alegomezc98@icloud.com>
2025-05-01 11:18:19 -0500

    remove some unused styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from 'next/font/google'
 import "./globals.scss";
-import LayoutWrapper from "./layout_wrapper";
 import RightClickBlocker from "./right_click_blocker";
+import { Box } from "@mui/material";
 
 const inter = Inter({
   variable: "--font-inter-sans",
@@ -36,14 +36,12 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} ${inter.variable}`}>
         {process.env.NODE_ENV === 'production' ? (
           <RightClickBlocker>
-            <LayoutWrapper>
               {children}
-            </LayoutWrapper>
           </RightClickBlocker>
         ) : (
-          <LayoutWrapper>
-            {children}
-          </LayoutWrapper>
+          <Box>
+            { children }
+          </Box>
         )}
 
       </body>
