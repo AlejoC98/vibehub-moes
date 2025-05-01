e5a53d01334235e37ba8d53b80094a03d5d35d72
Alejandro Gomez <alegomezc98@icloud.com>
2025-05-01 11:38:02 -0500

    Couple more updates regarding the background style for login main and dashboard main

---
+++ src/app/layout_wrapper.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { usePathname } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+
+export default function LayoutWrapper({ children }: { children: ReactNode }) {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const body = document.body
+    const isLoginPage = pathname === '/auth/login' || pathname === '/auth/reset-password'
+
+    if (isLoginPage) {
+      body.classList.add('login-bg')
+      body.classList.remove('dashboard-bg')
+    } else {
+      body.classList.add('dashboard-bg')
+      body.classList.remove('login-bg')
+    }
+
+    return () => {
+      body.classList.remove('login-bg')
+      body.classList.remove('dashboard-bg')
+    }
+  }, [pathname])
+
+  return <>{children}</>
+}
